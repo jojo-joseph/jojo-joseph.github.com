8cf03110ec9381ab5cea7d84ad68b63dac4dfdb0
jojo-joseph <jojojoseph1996@gmail.com>
2019-08-30 19:14:37 +0530

    react profile updated

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <div className="header">
+         
+        </div>
+        <div className="contentpage"><br/>
+             <div className="name">JOJO JOSEPH</div>  <br/>
+             <div className="text">Phone Number: +91-9074007159</div>
+             <div className="text">Gmail : jojojoseph1996@gmail.com</div>
+             <br/>
+             <div className="url">GitHub URL : https://github.com/jojo-joseph</div>
+             <div className="url">Stack Overflow : https://stackoverflow.com/users/9931618/jojo-joseph</div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
